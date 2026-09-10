9dd4ea6c6a6a50aceb67a2ad2d9070451925737e
Rohit Singh Sengar <rsengar257@gmail.com>
2026-09-10 22:54:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+function manipulateData() {
+    const output = document.getElementById("output");
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 0);
+    })
+    .then((arr) => {
+        return new Promise((resolve) => {
+            const evenNumbers = arr.filter(num => num % 2 === 0);
+
+            setTimeout(() => {
+                output.textContent = evenNumbers;
+                resolve(evenNumbers);
+            }, 1000);
+        });
+    })
+    .then((evenNumbers) => {
+        return new Promise((resolve) => {
+            const multiplied = evenNumbers.map(num => num * 2);
+
+            setTimeout(() => {
+                output.textContent = multiplied;
+                resolve(multiplied);
+            }, 2000);
+        });
+    });
+}
+manipulateData();
